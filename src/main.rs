255127ba19992662f730062d830ac61fe81d0077
José Relvas <josemonsantorelvas@gmail.com>
2024-09-30 00:21:52 +0100

    Fix path resolution
    Signed-off-by: José Relvas <josemonsantorelvas@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@
 
 mod sys;
 mod capabilities;
+mod logging;
 
 use core::str;
 use core::ffi::c_char;
@@ -35,6 +36,7 @@ use core::slice;
 
 use memchr::{memchr, memrchr};
 use arrayvec::ArrayString;
+use logging::PrintBuff;
 
 mod exit_code {
     macro_rules! def {
@@ -136,7 +138,7 @@ fn resolve_path(cwd_fd: i32, path: &str, buffer: &mut [u8]) -> usize {
     let mut fd_path = [0; 1024];
     let mut writer = PrintBuff::new(&mut fd_path);
 
-    _ = tfmt::uwrite!(&mut writer, "proc/self/fd/{}", fd);
+    _ = tfmt::uwrite!(&mut writer, "/proc/self/fd/{}\0", fd);
 
     let c_str = unsafe { CStr::from_bytes_with_nul_unchecked(&fd_path) };
 
@@ -280,63 +282,4 @@ pub extern fn main(_argc: i32, argv: *const *const c_char, envp: *const *const c
     }
 
     abort!(exit_code::TARGET_NO_VIABLE_BINARIES, "Target program has no viable binaries! Is it installed properly?")
-}
-
-struct PrintBuff<'a> {
-    buf: &'a mut [u8],
-    offset: usize,
-}
-
-impl<'a> PrintBuff<'a> {
-    fn new(buf: &'a mut [u8]) -> Self {
-        PrintBuff {
-            buf,
-            offset: 0,
-        }
-    }
-}
-
-impl<'a> tfmt::uWrite for PrintBuff<'a> {
-    type Error = ();
-
-    fn write_str(&mut self, s: &str) -> Result<(), ()> {
-        let bytes = s.as_bytes();
-
-        unsafe {
-            // Skip over already-copied data
-            let remainder = self.buf.get_unchecked_mut(self.offset..);
-            // Check if there is space remaining (return error instead of panicking)
-            if remainder.len() < bytes.len() { return Err(()); }
-            // Make the two slices the same length
-            let remainder = remainder.get_unchecked_mut(..bytes.len());
-            // Copy
-            remainder.copy_from_slice(bytes);
-
-            // Update offset to avoid overwriting
-            self.offset += bytes.len();
-        }
-
-        Ok(())
-    }
-}
-
-#[macro_export] macro_rules! abort {
-    ($exit_code:expr) => {{
-        print!(b"An error has occured!\n");
-        sys::exit($exit_code)
-    }};
-    ($exit_code:expr, $($arg:tt)*) => {{
-        print!($($arg)*);
-        sys::exit($exit_code)
-    }}
-}
-
-#[macro_export] macro_rules! print {
-    ($($arg:tt)*) => {{
-        let mut buffer = [0; 1024];
-        let mut writer = PrintBuff::new(&mut buffer);
-        _ = tfmt::uwriteln!(&mut writer, $($arg)*);
-
-        write_message!(&mut buffer);
-    }}
 }
