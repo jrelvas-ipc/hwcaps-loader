ec0c9d47ab947a1cb08e6d564ac22c9c13bb8a81
José Relvas <josemonsantorelvas@gmail.com>
2024-10-26 00:46:30 +0100

    src/init/arch_x86.rs: Update to use naked_asm! macro
    Signed-off-by: José Relvas <josemonsantorelvas@gmail.com>

--- src/init/arch_x86.rs
+++ src/init/arch_x86.rs
@@ -5,11 +5,10 @@ pub unsafe extern "C" fn _start() -> ! {
     // argument, a null return address, a null frame pointer, and an aligned
     // stack pointer. On many architectures, the incoming frame pointer is
     // already null.
-    core::arch::asm!(
+    core::arch::naked_asm!(
         "mov rdi, rsp", // Pass the incoming `rsp` as the arg to `entry`.
         "push rbp",     // Set the return address to zero.
         "jmp {entry}",  // Jump to `entry`.
         entry = sym crate::init::_main_proxy,
-        options(noreturn),
     )
 }
